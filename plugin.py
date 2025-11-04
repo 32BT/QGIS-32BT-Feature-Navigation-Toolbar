85cb4e06210c6bff9c21e87bcd50b2fc813191c7
32BT <81646793+32BT@users.noreply.github.com>
2025-11-04 20:35:55 +0100

    Add files via upload

---
+++ plugin.py
@@ -0,0 +1,25 @@
+
+################################################################################
+### Plugin Main
+################################################################################
+'''
+Note: unload may be called from a secundary loop like the plugin dialog.
+Deleting a toolbar in a secundary loop will not properly remove it from
+all UI references, specifically view->toolbars, hence: deleteLater()
+'''
+
+from .controller import NavigationController
+
+class Plugin:
+    def __init__(self, iface):
+        self._iface = iface
+
+    def initGui(self):
+        self._toolBar = self._iface.addToolBar("Feature Navigation Toolbar")
+        self._controller = NavigationController(self._iface, self._toolBar)
+
+    def unload(self):
+        self._controller = None
+        self._toolBar.deleteLater()
+
+################################################################################
