40ac22f78dfe5f7c3ea8946d1fe89801d74d9f9e
32BT <81646793+32BT@users.noreply.github.com>
2025-11-04 20:52:06 +0100

    Add files via upload

---
+++ controller/controller.py
@@ -0,0 +1,113 @@
+
+from qgis.PyQt.QtCore import *
+
+from .toolsetcontrollers import ResetController
+from .toolsetcontrollers import IndexController
+from .dialog import ResetDialog
+
+'''
+NavigationController
+    ResetController
+        ResetTools
+    IndexController
+        IndexTools
+'''
+
+################################################################################
+### KeyController
+################################################################################
+'''
+A KeyController is a controller that is publicly reachable from the dynamic
+properties of iface using a key. By default, there can be only one instance
+of any subclass of KeyController. The construct is meant to be used for
+*key* controllers, hence the name. (If more than one instance is ever desired,
+then a custom key can be provided.)
+
+Note that an unknown Python object offered to setProperty is likely stored as
+an integer. It is therefore not a true weak reference, but will not increase
+the refcount either. That means:
+    - it is not a circular reference, so __del__ will eventually be called
+    - we do need to clear the stored reference ourselves.
+'''
+class KeyController(QObject):
+    def __init__(self, iface, key=None):
+        super().__init__()
+        self._KEY = key or ("32bt."+self.__class__.__name__)
+        self._iface = iface
+        self._iface.setProperty(self._KEY, self)
+
+    def __del__(self):
+        self._iface.setProperty(self._KEY, None)
+        if hasattr(super(), '__del__'): super().__del__()
+
+    def find(self, key, alt=None):
+        return self._iface.property("32bt."+key) or alt
+
+################################################################################
+'''
+The main controller manages the relation between a ResetController and an
+IndexController. It is also a KeyController and therefore reachable via a key:
+
+    navCtl = self._iface.property("32bt.NavigationController")
+'''
+class NavigationController(KeyController):
+    didSelectFeature = pyqtSignal(object)
+
+    def __init__(self, iface, toolBar):
+        super().__init__(iface)
+        self._resetController = ResetController(iface, toolBar)
+        self._indexController = IndexController(iface, toolBar)
+
+        self._resetController.setDelegate(self)
+        self._indexController.didSelectFeature.connect(self.didSelectFeature)
+
+    ########################################################################
+    ### API
+    ########################################################################
+    '''
+    This will be called from outside the plugin context. This will:
+        - check if incoming layer corresponds to our current layer
+        - if so, update selection
+        - otherwise remove selection (caller responsibility?)
+    '''
+    def activeLayer(self):
+        return self._indexController.layer()
+
+    def selectNextFeature(self, layer):
+        if layer and layer.isValid():
+            if self._indexController.layer() == layer:
+                return self._indexController.selectNextFeature()
+            layer.removeSelection()
+
+    ########################################################################
+    ### ResetController delegation
+    ########################################################################
+
+    def validateReset(self, layer):
+        enable = self._indexController.validateLayer(layer)
+        self._resetController.setEnabled(enable)
+
+    def resetClicked(self, layer):
+        if self._indexController.validateLayer(layer):
+            if self.confirmReset(layer):
+                self._indexController.setLayer(layer)
+
+    ########################################################################
+    '''
+    Resetting the current session is potentially prohibitive. The user may have
+    had an elaborate selection set for browsing. Recreating the selection may
+    be expensive. We therefore want to double check a reset.
+
+    TODO some scenarios might even require a disabled/locked reset button?
+    '''
+    def confirmReset(self, new_layer):
+        old_layer = self._indexController.layer()
+        if old_layer is None:
+            # Reset from None means there was no active session
+            return True
+        else:
+            # There is an active session, ask confirmation
+            parent = self._iface.mainWindow()
+            return ResetDialog(parent).confirmReset(old_layer, new_layer)
+
+
