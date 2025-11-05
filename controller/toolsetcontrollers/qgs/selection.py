e6dbabf44e0361d5717157c3a1727e39c3035d41
32BT <81646793+32BT@users.noreply.github.com>
2025-11-05 14:44:05 +0100

    Add files via upload

---
+++ controller/toolsetcontrollers/qgs/selection.py
@@ -0,0 +1,55 @@
+
+from qgis.PyQt.QtCore import *
+
+################################################################################
+### Selection
+################################################################################
+'''
+For updating the resetbutton, we need to know if the currently active layer has
+a selection. Unfortunately, mapcanvas selectionChanged only emits a signal if
+the layer is currently visible, and subsequently changing the visibility does
+not emit any signal.
+
+So, we need to implement an alternative...
+
+The signal selection.changed will emit when:
+- the layerselection is changed, or
+- the featureselection is changed (regardless of visibility).
+The layer property may be None.
+'''
+
+class Selection(QObject):
+    changed = pyqtSignal(object)
+
+    def __init__(self, iface):
+        super().__init__()
+        self._iface = iface
+        self._iface.currentLayerChanged.connect(self.currentLayerChanged)
+        self._layer = None
+
+    def __del__(self):
+        self._iface.currentLayerChanged.disconnect(self.currentLayerChanged)
+        self._iface = None
+
+    def currentLayerChanged(self, layer):
+        self._layer = None
+        if hasattr(layer, 'selectionChanged'):
+            self._layer = Layer(layer)
+            self._layer.selectedFeaturesChanged.connect(self.changed)
+        self.changed.emit(layer)
+
+################################################################################
+
+class Layer(QObject):
+    selectedFeaturesChanged = pyqtSignal(object)
+
+    def __init__(self, layer):
+        super().__init__()
+        self._layer = layer
+        self._layer.selectionChanged.connect(self.selectionChanged)
+
+    def selectionChanged(self, *args):
+        self.selectedFeaturesChanged.emit(self._layer)
+
+################################################################################
+
