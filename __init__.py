6860b5b5c0f98b16b81d8956dce8a5f1b577dfdb
32bt <32beat@gmail.com>
2025-12-21 11:56:16 +0100

    Restructuring after move controller

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 
-
 class IDENTITY:
     AUTHOR = '32bt'
     MODULE = 'fnt'
     PREFIX = AUTHOR+'.'+MODULE+'.'
 
+class LANGUAGE:
+    from .language import _str as STR
 
 def classFactory(iface):
     from .plugin import Plugin
