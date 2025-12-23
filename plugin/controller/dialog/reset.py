5a9d701637fd0683b9143ee17710732788de649e
32bt <32beat@gmail.com>
2025-12-23 18:12:04 +0100

    Separate sampleBox from Resetdialog

--- plugin/controller/dialog/reset.py
+++ plugin/controller/dialog/reset.py
@@ -5,6 +5,7 @@ from qgis.PyQt.QtGui import *
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtWidgets import QDialog
 
+from .samplebox import SampleBox
 
 ################################################################################
 
@@ -28,11 +29,8 @@ _MOD = sys.modules.get(__name__.split('.')[0])
 _STR = _MOD.LANGUAGE.STR
 
 _TITLE = _STR("Reset Navigation")
-_SELECT_INFO = _STR("You have {} features selected for navigation in layer '{}'.")
-_SAMPLE_BOX = _STR("Random sample")
-_SAMPLE_INFO = _STR("Select or enter the desired samplesize below.")
-_SAMPLE_SIZE = _STR("Samplesize:")
-_SAMPLE_COUNT = _STR("Count:")
+_LABEL = _STR("You have {} features selected for navigation in layer '{}'.")
+_CHECK = _STR("Random sample")
 
 class Dialog(QDialog, _form()):
 
@@ -41,47 +39,21 @@ class Dialog(QDialog, _form()):
         self.setupUi(self)
         # Ensure translated labels
         self.setWindowTitle(_TITLE)
-        self.sampleBox.setTitle(_SAMPLE_BOX)
-        self.sampleInfo.setText(_SAMPLE_INFO)
-        self.sampleComboLabel.setText(_SAMPLE_SIZE)
-        self.sampleCountLabel.setText(_SAMPLE_COUNT)
-        validator = self.Validator()
-        self.sampleCombo.setValidator(validator)
-        self.sampleCombo.lineEdit().setValidator(validator)
-        self.sampleCount.setValidator(self.IntValidator())
+        self.sampleCheckBox.setText(_CHECK)
+        self.sampleCheckBox.toggled.connect(self.sampleCheckBoxToggled)
+        self.sampleBox = None
 
-        self.sampleCombo.currentTextChanged.connect(self.sampleComboChanged)
-        self.sampleCount.textEdited.connect(self.sampleCountChanged)
-        self.sampleCount.editingFinished.connect(self.sampleCountFinished)
+    def sampleCheckBoxToggled(self, state):
+        if not self.sampleBox:
+            self.sampleBox = SampleBox(self.maxSize)
+            self.layout().insertWidget(3, self.sampleBox)
+        self.sampleBox.setEnabled(state)
 
-
-    def sampleComboChanged(self, txt):
-        self.controlChanged(self.sampleCombo)
-
-    def sampleCountChanged(self):
-        self.controlChanged(self.sampleCount)
-
-    def sampleCountFinished(self):
-        self.setSize(self.getSize())
-
-    def controlChanged(self, sender):
-        maxSize = self._layer.selectedFeatureCount()
-        if sender == self.sampleCount:
-            n = self.getSize()
-            p = round(100 * n / self.maxSize())
-            with QSignalBlocker(self.sampleCombo):
-                self.setPercentage(p)
-        elif sender == self.sampleCombo:
-            p = self.getPercentage()
-            n = (p * self.maxSize() + 99) // 100
-            with QSignalBlocker(self.sampleCount):
-                self.setSize(n)
-
-
-
+    ########################################################################
+    ### Entrypoint
+    ########################################################################
     def confirmReset(self, layer):
-        size = self.setLayer(layer)
-        self.setSize(size)
+        self.setLayer(layer)
         if self.exec():
             return self.getSize()
 
@@ -89,75 +61,14 @@ class Dialog(QDialog, _form()):
         self._layer = layer
         name = layer.name()
         size = layer.selectedFeatureCount()
-        self.selectionInfo.setText(_SELECT_INFO.format(size, name))
-        self.sampleCount.setValidator(self.IntValidator(2, size))
-        return size
-
-    def altKeyActive(self):
-        modifiers = QgsApplication.instance().keyboardModifiers()
-        return bool(modifiers & Qt.AltModifier)
-
-    def getPercentage(self):
-        try:
-            txt = self.sampleCombo.currentText()
-            return int(txt.replace("%", "") or 0)
-        except Exception as error:
-            return 100
-
-    def setPercentage(self, p):
-        self.sampleCombo.setCurrentText(str(p)+"%")
-
-    def setSize(self, size):
-        size = self.limitSize(size)
-        self.sampleCount.setText(str(size))
+        self.selectionInfo.setText(_LABEL.format(size, name))
+        self.maxSize = size
 
     def getSize(self):
-        if not self.sampleBox.isChecked():
-            return self.maxSize()
-        try:
-            size = int(self.sampleCount.text() or 0)
-            return self.limitSize(size)
-        except Exception:
-            return self.maxSize()
+        if self.sampleCheckBox.isChecked():
+            return self.sampleBox.getSize()
+        else:
+            return self.maxSize
 
-    def limitSize(self, size):
-        return min(max(2, size), self.maxSize())
-
-    def maxSize(self):
-        maxSize = 2
-        if hasattr(self, '_layer'):
-            if self._layer and self._layer.isValid():
-                maxSize = self._layer.selectedFeatureCount()
-        return maxSize
-
-    ########################################################################
-    ### Validators
-    ########################################################################
-    '''
-    Returning state "Intermediate" will not trigger editingFinished,
-    so we allow empty text as "Acceptable". It does mean that "getSize"
-    should be prepared for empty text.
-    '''
-    class IntValidator(QIntValidator):
-        def validate(self, txt, pos):
-            state, txt, pos = super().validate(txt, pos)
-            if state != self.State.Invalid:
-                state = self.State.Acceptable
-            return state, txt, pos
-
-
-    class Validator(QValidator):
-        def validate(self, txt, pos):
-            if not txt:
-                state = self.State.Intermediate
-            elif self.acceptable(txt):
-                state = self.State.Acceptable
-            else:
-                state = self.State.Invalid
-            return (state, txt, pos)
-
-        def acceptable(self, txt):
-            try: return 0 < int(txt.replace("%","")) <= 100
-            except Exception: return False
 
 
