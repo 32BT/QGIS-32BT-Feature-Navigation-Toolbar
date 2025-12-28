a57b785fce59adb2bf11552f667e62ca74272963
32bt <32beat@gmail.com>
2025-12-28 21:06:53 +0100

    Restructuring language

--- language/__init__.py
+++ language/__init__.py
@@ -2,45 +2,41 @@
 
 from qgis.core import QgsApplication
 
-_dictionary = None
 
 ################################################################################
-### _str
+### Labels
 ################################################################################
 '''
+For translated labels by reference:
+    _LABELS = LABELS(...)
+    _LABELS.CUSTOM_LABEL1
+
+For direct translation by text:
+    _LABELS = LABELS(...)
+    translated_label = _LABELS("Custom Label 1")
+
 '''
-class String(str):
-    language = QgsApplication.instance().locale()
+import os, json
 
-    def __new__(cls, txt):
-        global _dictionary
-        if _dictionary is None:
-            _dictionary = Dictionary()
-        try:
-            key = txt.strip()
-            val = _dictionary[key]
-            txt = txt.replace(key, val)
-            return str.__new__(cls, txt)
-        except KeyError:
-            # return the input, since we don't know source-language
-            return txt
+class LABELS(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        super().update(self.loadLanguage() or {})
 
-################################################################################
-### Library
-################################################################################
+    def __call__(self, k):
+        return self.__getattr__(k) or k or ""
 
-import os
+    def __getattr__(self, k):
+        v = self.get(k)
+        if isinstance(v, list):
+            v = '\n'.join(v)
+        return v or ""
 
-class Dictionary(dict):
-    def __init__(self, lang=None):
-        super().__init__()
+    @classmethod
+    def loadLanguage(cls, lang=None):
         lang = lang or QgsApplication.instance().locale()
         path = os.path.dirname(__file__)
-        path = os.path.join(path, str(lang).lower()+'.txt')
+        path = os.path.join(path, str(lang).lower()+'.json')
         if os.path.exists(path):
             with open(path, encoding='utf-8') as f:
-                rows = list(f)
-            rows = [row.strip() for row in rows]
-            rows = [row for row in rows if row and row[0] != '#']
-            self.update(zip(rows[0::2],rows[1::2]))
-
+                return json.loads(f.read())
